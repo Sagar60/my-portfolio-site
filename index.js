57fa083f76cf63be8bb347aa49751fe8c329422b
Sagar60 <sagardaa058@gmail.com>
2022-10-04 12:49:50 +0530

    second

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const http =require('http');
+
+const app = express();
+
+//to load current directory all files
+app.use(express.static(__dirname))
+
+// render index.html file
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname +"\\"+"index.html");
+})
+
+const port = process.env.PORT | 8080;
+
+const server = http.createServer(app);
+
+server.listen(port,()=>{
+    console.log(__dirname);
+    console.log(`server started ar ${port}`);
+})
