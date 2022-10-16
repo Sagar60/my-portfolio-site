bf8799719761cbb5b4fa2ba252e3021716691acb
Sagar60 <sagardaa058@gmail.com>
2022-10-16 23:06:34 +0530

    commit by uipath

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const http =require('http');
+const morgan = require('morgan');
+const path = require('path');
 
 // refresh auto when changes happend in js
 // npm i -g nodemon
@@ -8,14 +10,44 @@ require(__dirname);
 const app = express();
 
 //to load current directory all files
-app.use(express.static(__dirname))
+app.use(express.static(__dirname));
+
+// to get log messages
+app.use(morgan('dev'));
 
 // render index.html file
 app.get('/',(req,res)=>{
     // res.sendFile(__dirname +"\\"+"index.html");
-    res.render(__dirname +"\\"+"index.html");
-})
+    res.sendFile(path.join(__dirname +'/index.html'));
+});
 
+// catch 404 and forward to error handler
+app.use(function(req,res,next){
+    const error = new Error('url Not found');
+    error.status = 404 ;
+    next(error);
+});
+
+// error handler
+app.use(function(err,req,res,next){
+    // console.log('error handler');
+    res.status(err.status || 500);
+    res.sendFile(path.join(__dirname +'/ErrorPage.html'));
+    
+    /*
+    res.status(err.status || 500 );
+    res.json({
+        error: err.message
+    });*/
+});
+
+/*
+app.use(function(req,res,next){
+    res.status(404).json({
+        error: 'bad url'
+    })
+})
+*/
 const port = process.env.PORT || 8080;
 
 // create server
